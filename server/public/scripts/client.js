91264e9dea9d2279d1c3755671ab60f8c2f4df2b
David Vinson <davidvinson018@gmail.com>
2024-05-10 15:24:06 -0500

    base is working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,71 @@
 function onReady() {
-  console.log("JavaScript is loaded!")
+  console.log('JavaScript is loaded!');
+  fetchGuess();
 }
 
-onReady()
+function postGuess(event) {
+  event.preventDefault();
+  console.log('post guess fired');
+
+  //player guessess
+  let player1Guess = document.getElementById('player-1').value;
+  let player2Guess = document.getElementById('player-2').value;
+
+  let playerGuess = {
+    player1: player1Guess,
+    player2: player2Guess,
+  };
+
+  // axios post to server
+
+  axios
+    .post('/guess', playerGuess)
+    .then((response) => {
+      // call fetchGuess to display
+      fetchGuess();
+    })
+    .catch((error) => console.error('error in post', error));
+}
+
+function fetchGuess() {
+  console.log('get guesses');
+  // axios get to server
+  axios
+    .get('/guess')
+    .then((response) => {
+      let playerData = response.data;
+      console.log('guess to display', playerData);
+      //display on DOM
+      let guessList = document.getElementById('guess-list');
+      guessList.innerHTML = '';
+      console.log('guessList', guessList);
+
+      //winner
+      let winner = document.getElementById('game-winner');
+      winner.innerHTML = '';
+      console.log('winner', winner);
+
+      playerData.forEach((element) => {
+        console.log('element', element);
+        if (element.player1_result === 'Winner' || element.player2_result === 'Winner') {
+          winner.innerHTML = `${element.player1_result === 'Winner' ? element.player1 : element.player2}`;
+        }
+        guessList.innerHTML += `
+        <li>player1: guess ${element.player1} score: ${element.player1_result}</li>
+        <li>player2: guess ${element.player2} score: ${element.player2_result}</li>
+        <hr/>
+        `;
+      });
+      // playerData.forEach(
+      //   (round) =>
+      //     (guessList += `
+      // <li>
+      //   player1: ${round.player1} -- score: ${round.player1_result}
+      //   player2: ${round.player2} -- score: ${round.player2_result}
+      // </li>`)
+      // );
+    })
+    .catch((error) => console.error('Error in fetching gueses', error));
+}
+
+onReady();
--- server/server.js
+++ server/server.js
@@ -1,17 +1,73 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json());
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// GET & POST Routes go here
+// let guess = { player1: '', player2: '' };
+let results = [];
 
+// GET & POST Routes go here
+app.get('/guess', (req, res) => {
+  console.log('get fired!');
+  res.send(results);
+});
+
+app.post('/guess', (req, res) => {
+  console.log('post fired');
+  // req.body should be {player1: '1', player2: '2'}
+  let playerGuess = req.body;
+  console.log('playerGuess', playerGuess);
+
+  //check player guess against a random number
+  let randomNumber = 5; //hard coded for test
+
+  //newObj to get player results
+  let playerResult = { ...playerGuess };
+  // handle player1
+  if (Number(playerGuess.player1) === randomNumber) {
+    //player wins!
+    playerResult.player1_result = 'Winner';
+  }
+  if (Number(playerGuess.player1) > randomNumber) {
+    // too high
+    playerResult.player1_result = 'Too High';
+  }
+  if (Number(playerGuess.player1) < randomNumber) {
+    // too low
+    playerResult.player1_result = 'Too Low';
+  }
+
+  // handle player2
+  if (Number(playerGuess.player2) === randomNumber) {
+    //player wins!
+    playerResult.player2_result = 'Winner';
+  }
+  if (Number(playerGuess.player2) > randomNumber) {
+    // too high
+    playerResult.player2_result = 'Too High';
+  }
+  if (Number(playerGuess.player2) < randomNumber) {
+    // too low
+    playerResult.player2_result = 'Too Low';
+  }
+
+  results.push(playerResult);
+
+  res.sendStatus(201);
+});
+
+app.post('/new_game', (req, res) => {
+  console.log('new game fired');
+  //req.body will be {}
+  results.length = 0;
+  res.sendStatus(201);
+});
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log('Server is running on port', PORT);
+});
